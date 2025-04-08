b5ece0f1a7136f7057bb0736b80d3cc69f0ebaa9
mstephens <mstephens.dev@gmail.com>
2025-04-08 09:29:20 -0400

    apr8_1

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Dominion_Unity-0.1.0";
+const contentToCache = [
+    "Build/web_apr8_1.loader.js",
+    "Build/web_apr8_1.framework.js",
+    "Build/web_apr8_1.data",
+    "Build/web_apr8_1.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
